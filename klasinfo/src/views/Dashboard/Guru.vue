09219d6174a920d4b1f0103663a9bd1aafca083c
verm024 <muhnaufal251@gmail.com>
2020-11-25 22:52:18 +0700

    Dashboard guru get daftar kelas

--- klasinfo/src/views/Dashboard/Guru.vue
+++ klasinfo/src/views/Dashboard/Guru.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="guru">
     Dashboard Guru
+    <div v-for="(item, index) in daftar_kelas" :key="index">
+      {{ item.nama }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import firebase from "../../firebase";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      daftar_kelas: []
+    };
+  },
+  computed: {
+    ...mapState(["currentUser"])
+  },
+  watch: {
+    get_daftar_kelas: {
+      immediate: true,
+      handler() {
+        this.$bind(
+          "daftar_kelas",
+          firebase.db
+            .collection("kelas")
+            .where(
+              "guru",
+              "==",
+              firebase.db.collection("users").doc(this.currentUser.uid)
+            )
+            .orderBy("nama")
+        );
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
